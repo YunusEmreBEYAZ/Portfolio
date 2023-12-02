06905912608993f693ae724402ceb36ada4c8e83
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-12-02 23:54:14 +0100

    working on sidebar

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import "./sidebar.css";
+import Logo from '../../assets/logo.png'
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <aside className='aside'>
+            <a href='#home' className='nav__logo'>
+                <img src={Logo} alt="Logo" />
+            </a>
+
+            <nav className="nav">
+                <div className="nav__menu">
+                    <ul className="nev__list">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link">
+                                <i className="icon-home"></i>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="" className="nav__link">
+                                <i className="icon-user-following"></i>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="" className="nav__link">
+                                <i className="icon-briefcase"></i>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="" className="nav__link"></a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="" className="nav__link"></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </aside>
     )
 }
 
