761e918d41c16d40efb3f819d95442a5f1de7d2b
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-12-03 00:49:27 +0100

    started home

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import "./home.css";
+import Me from "../../assets/me.png"
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className="section home container" id="home">
+            <div className="intro">
+                <img src={Me} alt="Me" className="home__img" />
+                <h1 className="home__name">Yunus Emre BEYAZ</h1>
+                <span className="home__education">I'm a Full Stack Web Developer</span>
+            </div>
+        </section>
     )
 }
 
