405e6c12a7179f2b5ee8b7eb244d79cc0f011045
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-12-04 01:14:30 +0100

    homepage structure

--- client/src/components/home/HeaderSocials.jsx
+++ client/src/components/home/HeaderSocials.jsx
@@ -14,6 +14,10 @@ const HeaderSocials = () => {
                 <i class="fa-brands fa-instagram fa-beat-fade"></i>
             </a>
 
+            <a href="https://www.twitter.com" className="home__social-link" target='_blank' rel='noreferrer'>
+                <i class="fa-brands fa-twitter fa-beat-fade"></i>
+            </a>
+
             <a href="https://www.facebook.com/yunusemreb1" className="home__social-link" target='_blank' rel='noreferrer'>
                 <i class="fa-brands fa-facebook fa-beat"></i>
             </a>
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./home.css";
-import Me from "../../assets/me.png"
+import Me from "../../assets/me.png";
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
     return (
@@ -9,6 +11,12 @@ const Home = () => {
                 <img src={Me} alt="Me" className="home__img" />
                 <h1 className="home__name">Yunus Emre BEYAZ</h1>
                 <span className="home__education">I'm a Full Stack Web Developer</span>
+
+                <HeaderSocials />
+
+                <a href="#contact" className="btn">Reach Me !</a>
+
+                <ScrollDown />
             </div>
         </section>
     )
