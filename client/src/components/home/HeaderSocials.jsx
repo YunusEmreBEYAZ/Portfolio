43ef445518b3c84338bf461d4be7fe8ed57c62ac
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-12-04 01:03:06 +0100

    home social media

---
+++ client/src/components/home/HeaderSocials.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+    return (
+        <div className="home__socials">
+            <a href="https://www.linkedin.com/in/yunus-emre-beyaz-410063253/" className="home__social-link" target='_blank' rel='noreferrer'>
+                <i class="fa-brands fa-linkedin fa-beat-fade"></i>
+            </a>
+            <a href="https://github.com/YunusEmreBEYAZ" className="home__social-link" target='_blank' rel='noreferrer'>
+                <i class="fa-brands fa-github fa-beat-fade"></i>
+            </a>
+
+            <a href="https://www.instagram.com/yemrebeyaz/" className="home__social-link" target='_blank' rel='noreferrer'>
+                <i class="fa-brands fa-instagram fa-beat-fade"></i>
+            </a>
+
+            <a href="https://www.facebook.com/yunusemreb1" className="home__social-link" target='_blank' rel='noreferrer'>
+                <i class="fa-brands fa-facebook fa-beat"></i>
+            </a>
+        </div>
+    )
+}
+
+export default HeaderSocials
