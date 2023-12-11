09e0322b42657a69fb1b37518dffc1d872ef7ef5
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-12-11 01:30:48 +0100

    resume finished

--- client/src/components/home/HeaderSocials.jsx
+++ client/src/components/home/HeaderSocials.jsx
@@ -4,22 +4,22 @@ const HeaderSocials = () => {
     return (
         <div className="home__socials">
             <a href="https://www.linkedin.com/in/yunus-emre-beyaz-410063253/" className="home__social-link" target='_blank' rel='noreferrer'>
-                <i class="fa-brands fa-linkedin fa-beat-fade"></i>
+                <i className="fa-brands fa-linkedin fa-beat-fade"></i>
             </a>
             <a href="https://github.com/YunusEmreBEYAZ" className="home__social-link" target='_blank' rel='noreferrer'>
-                <i class="fa-brands fa-github fa-beat-fade"></i>
+                <i className="fa-brands fa-github fa-beat-fade"></i>
             </a>
 
             <a href="https://www.instagram.com/yemrebeyaz/" className="home__social-link" target='_blank' rel='noreferrer'>
-                <i class="fa-brands fa-instagram fa-beat-fade"></i>
+                <i className="fa-brands fa-instagram fa-beat-fade"></i>
             </a>
 
             <a href="https://www.twitter.com" className="home__social-link" target='_blank' rel='noreferrer'>
-                <i class="fa-brands fa-twitter fa-beat-fade"></i>
+                <i className="fa-brands fa-twitter fa-beat-fade"></i>
             </a>
 
             <a href="https://www.facebook.com/yunusemreb1" className="home__social-link" target='_blank' rel='noreferrer'>
-                <i class="fa-brands fa-facebook fa-beat"></i>
+                <i className="fa-brands fa-facebook fa-beat"></i>
             </a>
         </div>
     )
