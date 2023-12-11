ddab2566fc78e924c69778d0952bb8cc4a351eee
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-12-11 01:00:02 +0100

    resume

---
+++ client/src/components/resume/Data.jsx
@@ -0,0 +1,52 @@
+const Data = [
+    {
+        id: 1,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2010-2014",
+        title: "Bachelor Degree",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ex enim quaerat repellat vitae ducimus, eaque quos quo natus eligendi atque totam ad veniam laudantium labore aperiam numquam blanditiis officiis!"
+    },
+    {
+        id: 2,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2017-2020",
+        title: "Bachelor Degree",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ex enim quaerat repellat vitae ducimus, eaque quos quo natus eligendi atque totam ad veniam laudantium labore aperiam numquam blanditiis officiis!"
+    },
+    {
+        id: 3,
+        category: "education",
+        icon: "icon-graduation",
+        year: "2022-2023",
+        title: "Trainee",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ex enim quaerat repellat vitae ducimus, eaque quos quo natus eligendi atque totam ad veniam laudantium labore aperiam numquam blanditiis officiis!"
+    },
+    {
+        id: 4,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2014-2016",
+        title: "Manager",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ex enim quaerat repellat vitae ducimus, eaque quos quo natus eligendi atque totam ad veniam laudantium labore aperiam numquam blanditiis officiis!"
+    },
+    {
+        id: 5,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2016-2020",
+        title: "Freelance",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ex enim quaerat repellat vitae ducimus, eaque quos quo natus eligendi atque totam ad veniam laudantium labore aperiam numquam blanditiis officiis!"
+    },
+    {
+        id: 6,
+        category: "experience",
+        icon: "icon-briefcase",
+        year: "2020-present",
+        title: "Front-End Developer",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla ex enim quaerat repellat vitae ducimus, eaque quos quo natus eligendi atque totam ad veniam laudantium labore aperiam numquam blanditiis officiis!"
+    }
+]
+
+export default Data
--- client/src/components/resume/Resume.jsx
+++ client/src/components/resume/Resume.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 import "./resume.css";
+import Data from './Data';
+import Card from './Card';
 
 const Resume = () => {
     return (
-        <div>Resume</div>
+        <section className="resume container section" id='resume'>
+            <h2 className="section__title">Experience</h2>
+            <div className="resume__container grid">
+                <div className="timeline grid">
+                    {Data.map((value, id) => {
+                        if (value.category === "education") {
+                            return (
+                                <Card
+                                    key={id}
+                                    icon={value.icon}
+                                    title={value.title}
+                                    year={value.year}
+                                    description={value.desc} />
+                            )
+                        }
+                    })}
+                </div>
+
+                <div className="timeline grid">
+                    {Data.map((value, index) => {
+                        if (value.category === "experience") {
+                            return (
+                                <Card
+                                    key={index}
+                                    icon={value.icon}
+                                    title={value.title}
+                                    year={value.year}
+                                    description={value.desc} />
+                            )
+                        }
+                    })}
+                </div>
+            </div>
+        </section>
     )
 }
 
