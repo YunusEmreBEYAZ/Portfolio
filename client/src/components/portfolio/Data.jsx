a2426698cdf7946fc221db1731e872351c556706
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-12-16 23:36:20 +0100

    added data for portfoliio

---
+++ client/src/components/portfolio/Data.jsx
@@ -0,0 +1,40 @@
+import Qa from "../../assets/qa.jpeg";
+import WeShare from "../../assets/weshare.png";
+import Todo from "../../assets/todo.png";
+import Guess from "../../assets/guess.png";
+import Dice from "../../assets/dice.png";
+
+const Data = [
+    {
+        id: 1,
+        image: Qa,
+        title: "Question-Answer App",
+        Category: "Project"
+    },
+    {
+        id: 2,
+        image: WeShare,
+        title: "Social Media App",
+        Category: "Project"
+    },
+    {
+        id: 3,
+        image: Todo,
+        title: "Todo App",
+        Category: "Project"
+    },
+    {
+        id: 4,
+        image: Guess,
+        title: "Guess My Number App",
+        Category: "Project"
+    },
+    {
+        id: 5,
+        image: Dice,
+        title: "Dice Game App",
+        Category: "Project"
+    }
+]
+
+export default Data;
