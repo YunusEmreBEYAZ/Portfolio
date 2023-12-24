1f2be09eeb37b0a5cc0596125459a9903b6e2cfa
Yunus Emre BEYAZ <yebeyaz@gmail.com>
2023-12-25 00:20:56 +0100

    added sending ail functionality

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./contact.css"
+import { useForm, ValidationError } from "@formspree/react";
+
 
 const Contact = () => {
+
+    const [state, handleSubmit] = useForm("mgegwqwj");
+    const formRef = useRef(null);
+
+    const resetForm = () => {
+        formRef.current.reset()
+    }
+
+    if (state.succeeded) {
+        resetForm();
+        return (
+            <section className="contact container section" id="contact">
+                <h2 className="section__title">Thank You for Your Mail!</h2>
+                <p>Thanks for your mail, I will contact with you soon!</p>
+            </section>
+        )
+    }
+
     return (
         <section className="contact container section" id='contact'>
             <h2 className="section__title">Reach Me Out</h2>
@@ -12,26 +32,69 @@ const Contact = () => {
                     <p className="contact__details">Send me an e-mail</p>
                 </div>
 
-                <form action="" className="contact__form">
+                <form ref={formRef} onSubmit={handleSubmit}
+                    className="contact__form">
                     <div className="contact__form-group">
                         <div className="contact__form-div">
-                            <input type="text" className="contact__form-input" placeholder='What is your name?' />
+                            <input
+                                type="text"
+                                name='name'
+                                className="contact__form-input"
+                                placeholder='What is your name?'
+                            />
+
+                            <ValidationError
+                                prefix="name"
+                                field="name"
+                                errors={state.errors}
+                            />
                         </div>
 
                         <div className="contact__form-div">
-                            <input type="email" className="contact__form-input" placeholder='What is your email?' />
+                            <input
+                                type="email"
+                                name='email'
+                                className="contact__form-input"
+                                placeholder='What is your email?'
+                            />
+
+                            <ValidationError
+                                prefix="Email"
+                                field="email"
+                                errors={state.errors}
+                            />
                         </div>
                     </div>
 
                     <div className="contact__form-div">
-                        <input type="text" className="contact__form-input" placeholder='What is the subject?' />
+                        <input
+                            type="text"
+                            name='subject'
+                            className="contact__form-input"
+                            placeholder='What is the subject?'
+                        />
+
+                        <ValidationError
+                            prefix="subject"
+                            field="subject"
+                            errors={state.errors}
+                        />
                     </div>
 
                     <div className="contact__form-div message-area">
-                        <textarea name="" id="" cols="30" rows="10" className='contact__form-input ' placeholder='Write your message here please'></textarea>
+                        <textarea
+                            name="message"
+                            cols="30" rows="10"
+                            className='contact__form-input '
+                            placeholder='Write your message here please'></textarea>
+                        <ValidationError
+                            prefix="message"
+                            field="message"
+                            errors={state.errors}
+                        />
                     </div>
 
-                    <button className="btn">Send</button>
+                    <button type='submit' disabled={state.submitting} className="btn">Send</button>
                 </form>
             </div>
         </section>
